8649bf80ac8311f8b85fdbec28f9b2ee826a6a68
aakarsh-kushwah <sayogkushwah777@gmail.com>
2025-11-10 20:41:24 +0530

    fix: remove unused AuthProvider

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext'; // ✅ AuthProvider इंपोर्ट करें
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
